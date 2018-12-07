e0e53c468d03fe7eb39e613e353c431f1571fea4
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-12-07 17:03:12 +0000

    Added user login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://rob:Robert1@ds239071.mlab.com:39071/devwebapp'
+    mongoURI: 'mongodb://rob:Robert1@ds239071.mlab.com:39071/devwebapp',
+    secretOrKey: 'secret'
 }
