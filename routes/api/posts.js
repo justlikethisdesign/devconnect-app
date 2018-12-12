7bb56a0a4057eead1acf1467eea7fde9b61f8335
Rob Marshall <rob@fox.agency>
2018-12-12 18:30:43 +0000

    Added Post validation

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Post model
 const Post = require('../../models/Post');
+
+// Validation
+const validatePostInput = require('../../validation/post');
 
 // @route   GET api/posts/test
 // @desc    Tests post route
@@ -14,6 +18,15 @@ router.get('/test', (req, res) => res.json({msg: "Posts Works"}));
 // @desc    Create post
 // @access  Private
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if(!isValid) {
+        // If any errors, send 400 with errors
+        return res.status(400).json(errors);
+    }
+
     const newPost = new Post({
         text: req.body.text,
         name: req.body.name,
