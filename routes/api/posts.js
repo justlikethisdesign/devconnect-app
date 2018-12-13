0b0232064a87cb2454ce4583fd76debb5ae77b4a
Rob Marshall <rob@fox.agency>
2018-12-13 13:50:48 +0000

    Delete comments api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -181,10 +181,23 @@ router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session
 
     Post.findById(req.params.id)
         .then(post => {
-            // Check to see if comment exists
-            if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0){
 
+            // Check to see if comment exists
+            if(post.comments.filter(
+                comment => comment._id.toString() === req.params.comment_id
+            ).length === 0){
+                return res.status(404).json({ commentnotexist: 'Comment does not exist' });
             }
+
+            // Get the remove index
+            const removeIndex = post.comments
+                .map(item => item._id.toString())
+                .indexOf(req.params.comment_id);
+
+            // Splice comment out of array
+            post.comments.splice(removeIndex, 1);
+
+            post.save().then(post => res.json( post ));
         })
         .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
 });
