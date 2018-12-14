c0e882836c81a476061f7221f81a2403e9e6c209
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-12-14 08:11:02 +0000

    Added login & register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 
@@ -9,11 +12,17 @@ import './App.css';
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Navbar />
-                <Landing />
-                <Footer />
-            </div>
+            <Router>
+                <div className="App">
+                    <Navbar />
+                    <Route exact path="/" component={Landing} />
+                    <div className="container">
+                        <Route exact path="/register" component={Register}></Route>
+                        <Route exact path="/login" component={Login}></Route>
+                    </div>
+                    <Footer />
+                </div>
+            </Router>
         );
     }
 }
