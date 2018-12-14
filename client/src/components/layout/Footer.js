1ba76515efb720530a20de17a957a86a2212af28
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-12-14 08:23:01 +0000

    Fixed footer bootstrap issues

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 
 const Footer = (props) => {
     return (
-        <footer className="footer.bg-dark.text.white.mt-5.p-4.text-center">
-            Copyright &copy; {new Date().getFullYear()} DevConnector
+        <footer class="bg-dark text-white mt-5 p-4 text-center">
+            Copyright &copy; 2018 Dev Connector
         </footer>
     )
 }
