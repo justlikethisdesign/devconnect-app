4c045068ab445e5ac407c5e0ec162b716d8c6f07
Rob Marshall <rob@fox.agency>
2018-12-14 12:50:36 +0000

    Added links

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 const Footer = (props) => {
     return (
-        <footer class="bg-dark text-white mt-5 p-4 text-center">
+        <footer className="bg-dark text-white mt-5 p-4 text-center">
             Copyright &copy; 2018 Dev Connector
         </footer>
     )
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -15,7 +15,7 @@ export default() => {
             <div className="collapse navbar-collapse" id="mobile-nav">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
-                  <Link className="nav-link" to="/profiles"> 
+                  <Link className="nav-link" to="/profiles">
                       Developers
                   </Link>
                 </li>
@@ -28,7 +28,7 @@ export default() => {
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="login">
+                  <Link className="nav-link" to="/login">
                       Login
                   </Link>
                 </li>
