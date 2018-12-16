343254edf9f7f90f81c4f0db5551d67e057ed550
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-12-16 11:04:48 +0000

    Added login action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -14,3 +14,22 @@ export const registerUser = (userData, history) => dispatch => {
             })
         );
 }
+
+// Login - get user token
+export const loginUser = (userData) => dispatch => {
+    axios.post('/api/user/login', userData)
+        .then(res =>){
+            // Save to local storage
+            const { token }= res.data;
+            // Set token to local
+            localStorage.setItem('jwtToken' token);
+            // Set token to auth header
+            setAuthToken(token);
+        })
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+}
