ae9860a15a14740c04621c243ff0414f7b8c2b73
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-12-28 16:42:56 +0000

    Added conctructor

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Link, withRouter } from 'react-router-dom';
+import { TextFieldGroup } from '../common/TextFieldGroup';
+import { TextAreaFieldGroup } from '../common/TextAreaFieldGroup';
+import { connect } from 'react-redux';
 
 class AddExperience extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            company: '',
+            title: '',
+            location: '',
+            from: '',
+            to: '',
+            current: false,
+            description: '',
+            errors: {},
+            disabled: false
+        }
+    }
+
     render () {
 
     }
 }
 
-export default AddExperience;
+export default connect()(AddExperience);
