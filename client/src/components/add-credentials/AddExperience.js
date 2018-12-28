de8f6c387ff177a2d540b44c0fe82d1bb1c502a8
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-12-28 16:42:20 +0000

    Added component

---
+++ client/src/components/add-credentials/AddExperience.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class AddExperience extends Component {
+    render () {
+
+    }
+}
+
+export default AddExperience;
