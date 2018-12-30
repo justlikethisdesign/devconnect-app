88c27fed90296dd50dd6cda77470c800803e6f9c
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-12-30 18:24:43 +0000

    Added delete profile action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -74,6 +74,23 @@ export const addEducation = (eduData, history) => dispatch => {
         );
 }
 
+// Delete Experence
+export const deleteExperience = id => dispatch => {
+    axios
+        .delete(`/api/profile/experience/${id}`)
+        .then(res => 
+            dispatch({
+                type: GET_PROFILE,
+                payload: res.data
+            })
+        )
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+}
 
 // Delete account and profile
 export const deleteAccount = () => dispatch => {
