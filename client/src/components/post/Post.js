2ac453466cc8ed53dd093a391730940d0277b815
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-01-04 17:18:44 +0000

    Added single post component

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PostItem from '../posts/PostItem';
 import Spinner from '../common/Spinner';
 import { getPost } from '../../actions/postActions';
 
@@ -11,8 +13,32 @@ class Post extends React.Component {
     }
 
     render () {
+        const { post, loading } = this.props.post;
+        let postContent;
+
+        if(post === null || loading || Object.keys(post).length === 0){
+            postContent = <Spinner />
+        } else {
+            postContent = (
+                <div>
+                    <PostItem post={post} showActions={false}/>
+                </div>
+            );
+        }
+
         return (
-            <h1>Post</h1>
+            <div className="post">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <Link to="/feed" className="btn btn-light mb-3">
+                                Back to feed
+                            </Link>
+                            {postContent}
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
