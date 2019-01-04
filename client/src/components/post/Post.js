ee46aff9f8ba9660794b8229df71f03527be37a6
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-01-04 17:55:26 +0000

    Imported comment feed

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import CommentForm from './CommentForm';
+import CommentFeed from './CommentFeed';
 import PostItem from '../posts/PostItem';
 import Spinner from '../common/Spinner';
 import { getPost } from '../../actions/postActions';
@@ -24,6 +25,7 @@ class Post extends React.Component {
                 <div>
                     <PostItem post={post} showActions={false}/>
                     <CommentForm postId={post._id} />
+                    <CommentFeed postId={post._id} comments={post.comments}/>
                 </div>
             );
         }
