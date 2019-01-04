a198b9df8e5500410d6064bb781221c782895557
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-01-04 15:41:03 +0000

    Added show post content

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,9 +3,25 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import PostForm from './PostForm';
 import Spinner from '../common/Spinner';
+import { getPosts } from '../../actions/postActions';
+import PostFeed from './PostFeed';
 
 class Posts extends React.Component {
+
+    componentDidMount() {
+        this.props.getPosts();
+    }
+
     render () {
+
+        const { posts, loading } = this.props.post;
+        let postContent;
+
+        if(posts === null || loading){
+            postContent = <Spinner />
+        } else {
+            postContent = <PostFeed posts={posts}/>
+        }
 
         return(
             <div className="feed">
@@ -13,6 +29,7 @@ class Posts extends React.Component {
                     <div className="row">
                         <div className="col-md-12">
                             <PostForm />
+                            {postContent}
                         </div>
                     </div>
                 </div>
@@ -22,4 +39,13 @@ class Posts extends React.Component {
     }
 }
 
-export default Posts;
+Posts.propTypes = {
+    getPosts: PropTypes.func.isRequired,
+    post: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    post: state.post
+});
+
+export default connect(mapStateToProps, { getPosts })(Posts);
